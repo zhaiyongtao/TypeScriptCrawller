4064d032f8333948a7862a0ee7b42d3a7ee50de8
yongtao zhai <501671900@qq.com>
2020-05-07 00:20:25 +0800

    fix：代码优化

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,66 +1,35 @@
 import superagent from "superagent";
-import cheerio from "cheerio"
-import  fs from 'fs';
-import  path from 'path';
-
-interface Course {
-    title:string;
-    count:number
-}
-
-interface CourseResult {
-    time: number;
-    data: Course[];
-}
-interface Content {
-    [propName: number] : Course[];
-}
+import zytAnalyzer from './zytAnalyzer';
+import path from "path";
+import fs from 'fs';
 
 class Crowller {
-    private secret = "secretKey";
-    private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
-    private rawHtml = '';
-
+    private filePath = path.resolve(__dirname, '../data/course.json');
     async getRawHtml() {
         const result: superagent.Response = await superagent.get(this.url);
         return result.text
-
     }
-    getCourseInfo(html:string) {
-        const $ = cheerio.load(html)
-        const courseItems = $('.course-item');
-        const courseInfos: Course[] = [];
-        console.log(courseItems.length)
-        courseItems.map((index, element) => {
-            const descs = $(element).find('.course-desc');
-            const title = descs.eq(0).text();
-            const count = parseInt(descs.eq(1).text()) ?parseInt(descs.eq(1).text()):0;
-            courseInfos.push({title, count})
-        })
-        return {
-            time: new Date().getTime(),
-            data:courseInfos
-        }
-    }
-    generateJsonContent(courseInfo:CourseResult) {
-        const filePath = path.resolve(__dirname,'../data/course.json');
-        let fileContent:Content = {};
-        if (fs.existsSync(filePath)) {
-            fileContent = JSON.parse(fs.readFileSync(filePath, "utf-8"))
-        }
-        fileContent[courseInfo.time] = courseInfo.data;
-        fs.writeFileSync(filePath,JSON.stringify(fileContent))
-    }
-
+    writeFile(content: string) {
+        fs.writeFileSync(this.filePath,content);
+      }
+    
     async initSpiderProcess () {
         const html = await this.getRawHtml()
-        const courseInfo = this.getCourseInfo(html);
-        this.generateJsonContent(courseInfo)
+        const fileContent =  this.analyzer.analyze(html, this.filePath);
+            this.writeFile(fileContent)
     }
-
-    constructor() {
+    // private analyzer;
+    // constructor (analyzer) {
+    //     this.analyzer = analyzer
+    // }
+    constructor(private analyzer:any,private url:string) {
         this.initSpiderProcess();
     }
 }
 
-const crowller = new Crowller();
+// 方法类实例化，使用实例化对象中的方法
+const secret = "secretKey";
+const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+const analyzer = new zytAnalyzer (); 
+new Crowller(analyzer,url);
+
